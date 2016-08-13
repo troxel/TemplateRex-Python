c8e9e3be8fedd6c1546d26615c4d7c61db6f0e9a
Steve Troxel <troxel@perlworks.com>
2016-08-12 18:11:36 -0700

    Completed implimentation of format() found problem though in that format syntax collides with css/javascript

--- trex/template.py
+++ trex/template.py
@@ -17,7 +17,6 @@ class TemplateRex:
     cmnt_prefix = '<!--'
     cmnt_postfix = '-->'
     func_prefix = '&'
-    id_prefix   = '\$'
         
     # ----------------------
     def __init__(self, **args):
@@ -27,33 +26,37 @@ class TemplateRex:
         #else: raise Exception('fname arguement required')
 
         self.cmnt_verbose = 1
-        self.func_registered = {}
-
-        self.tsections = {}         # template sections
-        self.tsections_funcs = {}   # template section function structs    
-
-        self.psections_str = { 'main':"", 'base':""}   # processed sections rendered str
-        self.psections_lst = { 'main':[], 'base':[]}   # processed sections as lst
-        self.csections = { 'main':[], 'base':[] }  # child sections
-        self.last_parent = ['main']     # used to determine last found parent in recursive func
+    
+        self.tblks={}
+ 
+        self.pblks_str = { 'BLK_MAIN':"", 'base':""}   # processed sections rendered str
+        self.pblks_lst = { 'BLK_MAIN':[], 'base':[]}   # processed sections as lst
+        self.cblks = { 'BLK_MAIN':[], 'base':[] }  # child sections
+        self.last_parent = ['BLK_MAIN']     # used to determine last found parent in recursive func
     
         self.block_pattern = self.cmnt_prefix + r'\s*BEGIN=(?P<nm>\w+)\s*' + self.cmnt_postfix + r'(?P<inner>.*?)' + self.cmnt_prefix + r'\s*END=\1 ' + self.cmnt_postfix
         self.base_pattern  = self.cmnt_prefix + r'\s*BASE=(?P<nm>\S+)\s*' + self.cmnt_postfix
-        #self.func_pattern  = self.func_prefix + r'(?P<fn_nm>\S+)\((?P<fn_args>.*?)\)'
         self.func_pattern  = self.func_prefix + r'({?[a-zA-Z][\w]+}?)\((.*?)\)'
-        self.id_pattern  = self.id_prefix + r'({?[a-zA-Z][\w]+}?)'
         
-        self.block_re = re.compile(self.block_pattern, re.DOTALL)
-        self.func_re  = re.compile(self.func_pattern)
-        self.id_re    = re.compile(self.id_pattern,re.DOTALL)
+        self.block_re  = re.compile(self.block_pattern, re.DOTALL)
+        self.func_re   = re.compile(self.func_pattern)
+        self.fld_re     = re.compile(r'{(\w+)\S*?}', re.DOTALL)
         self.quotes_re = re.compile('[\'\"]')
-        self.id_prefix_re = re.compile(r'^\$')
+
+        self.BLK_MAIN_pre  = "{0} {1} {2}".format(self.cmnt_prefix,"BEGIN=BLK_MAIN",self.cmnt_postfix)
+        self.BLK_MAIN_post = "{0} {1} {2}".format(self.cmnt_prefix,"END=BLK_MAIN",self.cmnt_postfix)
 
         for key in args.keys():
             self.__dict__[key] = args[key]
             
         # load template based on a search list
         self.get_template(self.fname)
+        
+        #print("***********")
+        #pp.pprint(self.tblks)
+        #pp.pprint(self.cblks)
+        #print("***********")
+        #sys.exit()
        
     # ----------------------
     def get_template(self,fname):
@@ -68,7 +71,7 @@ class TemplateRex:
                 if os.path.isfile(fspec_msh):
                     if os.stat(fspec_msh).st_mtime > os.stat(fspec_msh).st_mtime:
                         fid = open(fspec_msh, 'rb')
-                        self.tsections = marshal.load(fid)
+                        self.tblks = marshal.load(fid)
                         fid.close()
                 else:
                     try: fid = open(fspec, 'r')
@@ -82,58 +85,64 @@ class TemplateRex:
                     if match:
                         fname_base = match.group('nm')
                         # Save to 'base' as this is rendered in the final render if present
-                        self.tsections['main'] = self.get_template(fname_base)
-                        self.tsections['main_child'] = self.parse_template(file_str)
+                        self.get_template(fname_base)
+                        self.parse_template(file_str)
                     else:
-                        self.tsections['main'] = self.parse_template(file_str)
+                        # If main template wrap in a main block for parse_template
+                        template_str = "{0}\n{1}\n{2}\n".format(self.BLK_MAIN_pre,file_str,self.BLK_MAIN_post)
+                        self.parse_template(template_str)
                     
-                    # Process functions populates self.tsections_funcs
+                    # Process functions populates self.tblks[blk_name]['funcs']
                     self.parse_functions()
                     
                     ### if compile_flg marshal tsections to fspec_msh
 
                 break
 
-        if not self.tsections:
+        if not self.tblks:
             print('No Template File %s Found' % (fname),'in search path -> ', ' , '.join(self.template_dirs))
             raise
-
-        return self.tsections['main']
 
     # ----------------------
     def parse_template(self, t_str):
 
         def parse_capture(obj):
-            name_sec = obj.group(1)
+            blk_name = obj.group(1)
             
-            self.csections[name_sec] = []
-            self.last_parent.append(name_sec)
+            self.cblks[blk_name] = []
+            self.last_parent.append(blk_name)
 
             proc_rtn = self.parse_template(obj.group(2))  # recursive call group(2) is next template section
 
             self.last_parent.pop()
-            self.csections[self.last_parent[-1]].append(name_sec)
+            self.cblks[self.last_parent[-1]].append(blk_name)
 
-            self.tsections[name_sec] = proc_rtn.strip()
-            self.psections_str[name_sec] = ""   # need to initialize to prevent key exceptions
-            self.psections_lst[name_sec] = []   # ...
+            # Assign and init to template blk structure  
+            self.tblks[blk_name] = {}
             
-            return "$" + name_sec
+            self.tblks[blk_name]['blk_str'] = proc_rtn.strip()
+            self.tblks[blk_name]['flds'] = self.fld_re.findall(self.tblks[blk_name]['blk_str'])  
+            self.tblks[blk_name]['funcs'] = {}
 
-        section = self.block_re.sub(parse_capture, t_str, re.DOTALL)
-        return section
+            self.pblks_str[blk_name] = ""   # need to initialize to prevent key exceptions
+            self.pblks_lst[blk_name] = []   # ...
+            
+            return "{" + blk_name + "}"
 
+        blk = self.block_re.sub(parse_capture, t_str, re.DOTALL)
+        return blk
+        
     # ----------------------
     def parse_functions(self):
 
         # ----------------------------
         def arg_study(arg):
      
-            (arg,cnt) = self.id_prefix_re.subn('',arg)
-            if cnt: return arg,True
+            (arg,cnt) = self.fld_re.subn('',arg)
+            if cnt: return arg,True                 # context
         
             (arg,cnt) = self.quotes_re.subn('',arg)
-            if cnt: return arg,False
+            if cnt: return arg,False                # not context 
         
             if arg == 'False': return(False,False)
             if arg == 'True':  return(True,False)
@@ -158,11 +167,8 @@ class TemplateRex:
             slug = re.sub('\W+',"",slug)
 
             func_ref = functions.FUNCTIONS[func_name]   # Should we check if exists or just default to something?
-           
-            if not section in self.tsections_funcs: 
-                self.tsections_funcs[section] = {}
             
-            self.tsections_funcs[section][slug] = {'args_ctx':[],'args':[],'args_rnd':[],'kwargs':{},'ref':func_ref}
+            self.tblks[blk_name]['funcs'][slug] = {'args_ctx':[],'args':[],'args_rnd':[],'kwargs':{},'ref':func_ref}
             
             # Determine nature of the args
             arg_pos_lst = [arg for arg in arg_lst if not re.search("=",arg)]
@@ -170,66 +176,80 @@ class TemplateRex:
             
             for inx,arg in enumerate(arg_pos_lst):
                 arg,ctx = arg_study(arg)
-                self.tsections_funcs[section][slug]['args'].append(arg)
-                self.tsections_funcs[section][slug]['args_rnd'].append(arg)  # rendering scratchpad
+                self.tblks[blk_name]['funcs'][slug]['args'].append(arg)
+                self.tblks[blk_name]['funcs'][slug]['args_rnd'].append(arg)  # rendering scratchpad
                 if ctx: 
-                    self.tsections_funcs[section][slug]['args_ctx'].append(inx)
+                    self.tblks[blk_name]['funcs'][slug]['args_ctx'].append(inx)
                    
-            # Assemble kwargs (note not facilitating context kwargs are this time)
+            # Assemble kwargs (note not facilitating context kwargs at this time)
             for arg in arg_kw_lst:
                 kw,val = arg.split("=")
                 val,ctx = arg_study(val)
-                self.tsections_funcs[section][slug]['kwargs'][kw] = val
+                self.tblks[blk_name]['funcs'][slug]['kwargs'][kw] = val
 
-            return "$" + slug
+            return "{" + slug + "}"
+        
+        for blk_name in self.tblks:                 
+            self.tblks[blk_name]['blk_str'] = self.func_re.sub(parse_capture, self.tblks[blk_name]['blk_str'] )
 
-        for section in self.tsections:                        
-            self.tsections[section] = self.func_re.sub(parse_capture, self.tsections[section], count=1)
-           
         return
             
     # ----------------------
-    def render_sec(self, section, context={}):
+    def render_sec(self, blk, context={}):
 
-        for child in self.csections[section]:
-            self.psections_str[child] = "\n".join(self.psections_lst[child])
-            self.psections_lst[child] = []
-                
+        if not blk in self.tblks: 
+            print("Warning no blk",blk,"found in template")
+            return
+                            
         if not isinstance(context, dict):
             context = context.__dict__
             
-        # Optimization: if no children then no sub-sections to update   
-        if self.csections[section]:
-            context.update(self.psections_str)
+        # Optimization: if no children then no sub-blks to update   
+        if self.cblks[blk]:
+            context.update(self.pblks_str)
+
+
+
+        for child in self.cblks[blk]:
+            #self.pblks_str[child] = "\n".join(self.pblks_lst[child])
+            self.pblks_str[child] = ""
+            self.pblks_lst[child] = []
+
 
         # Function processing
-        if section in self.tsections_funcs:
-            for key_slug in self.tsections_funcs[section]:
-                func_args = self.tsections_funcs[section][key_slug]['args_rnd'] 
+        for slug in self.tblks[blk]['funcs']:
+            func_args = self.tblks[blk]['funcs'][slug]['args_rnd'] 
                
-                for inx in self.tsections_funcs[section][key_slug]['args_ctx']:
-                    func_args[inx] = context[self.tsections_funcs[section][key_slug]['args'][inx]]
+            for inx in self.tblks[blk]['funcs'][slug]['args_ctx']:
+                func_args[inx] = context[self.tblks[blk]['funcs'][slug]['args'][inx]]
                                                             
-                func_kwargs = self.tsections_funcs[section][key_slug]['kwargs']
-                context[key_slug] = self.tsections_funcs[section][key_slug]['ref'](*func_args,**func_kwargs)
+            func_kwargs = self.tblks[blk]['funcs'][slug]['kwargs']
+            context[slug] = self.tblks[blk]['funcs'][slug]['ref'](*func_args,**func_kwargs)
 
-        # Var substitution       
-        if section == 'main':
-            self.psections_str[section] = self.subtitute_var(self.tsections[section],context)
+        # Fld substitution
+        if blk == 'BLK_MAIN':
+            self.pblks_str[blk] = self.subtitute_fld(self.tblks[blk]['blk_str'],context)
         else: 
-            self.psections_lst[section].append( self.subtitute_var(self.tsections[section],context) ) 
-
-        return(self.psections_str[section])
+            #self.pblks_lst[blk].append( self.subtitute_fld(self.tblks[blk]['blk_str'],context) ) 
+            self.pblks_str[blk] += self.subtitute_fld(self.tblks[blk]['blk_str'],context) 
+            
+        return(self.pblks_str[blk])
 
     # ----------------------
     def render(self, context={}):
-        return self.render_sec("main", context)
+        return self.render_sec('BLK_MAIN', context)
 
     # ----------------------
-    def subtitute_var(self, str_in, context):
+    def subtitute_fld(self, str_in, context):
 
+        rtn = str_in.format(**context)
+        return rtn
+
+# ----------------------
+    def subtitute_var(self, str_in, context):
+        _str = str
         def process_capture(obj):
-            try: return str(context[obj.group(1)])
+            try: return _str(context[obj.group(1)])
             except: return "" 
 
         rtn = self.id_re.sub(process_capture, str_in)

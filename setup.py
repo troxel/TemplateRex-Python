69982c34bcdff3787ce0681e22a3c47a1f40c79d
root <root@peppermint.netgear.com>
2018-11-18 11:14:56 -0800

    setup.py changes to faciliate PYPI

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 
-def readme():
-    with open('README.rst') as f:
-        return f.read()
+from setuptools import setup, find_packages
+from os import path
+from io import open
 
-from distutils.core import setup
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setup(name='TemplateRex',
       version='1.0',
       packages=['templaterex'],
@@ -12,8 +18,9 @@ setup(name='TemplateRex',
       author='Steve Troxel',
       author_email='troxel@perlworks.com',
       description='KISS Pure Logicless Template Engine',
-      long_description=readme(),
-      keywords="template web html text",
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      keywords="template web html text logicless",
       url='https://github.com/troxel/TemplateRex-Python',
       include_package_data=False,
       install_requires=['MarkupSafe'],
@@ -25,8 +32,14 @@ setup(name='TemplateRex',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Text Processing :: Markup :: HTML'
     ],
+    project_urls={ 
+        'Bug Reports': 'https://github.com/troxel/TemplateRex-Python/issues',
+        'Source': 'https://github.com/troxel/TemplateRex-Python',
+    },
 )
